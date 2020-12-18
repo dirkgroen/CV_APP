ab0b124a23e956e42509b5ba15d7c369f131476c
Groen, Dirk <dgroen@ilionx.com>
2020-12-18 15:16:11 +0100

    nieuwe poging

--- cv-app-sqldb/External Resources/EXTDS_CV_Blob_ID.sql
+++ cv-app-sqldb/External Resources/EXTDS_CV_Blob_ID.sql
@@ -1,6 +1,6 @@
 ﻿CREATE EXTERNAL DATA SOURCE [EXTDS_CV_Blob_ID]
     WITH (
-    TYPE = 'BLOB_STORAGE',
+    TYPE = BLOB_STORAGE,
     LOCATION = N'https://cvappstorage.blob.core.windows.net/api-json-id',
     CREDENTIAL = [SC_SAS_CV_APP]
     );
