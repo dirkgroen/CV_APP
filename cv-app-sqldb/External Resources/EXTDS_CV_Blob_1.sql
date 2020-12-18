3bc63967992a833a52c6e67a65a27b940a24af06
Groen, Dirk <dgroen@ilionx.com>
2020-12-18 14:04:26 +0100

    build fix

--- cv-app-sqldb/External Resources/EXTDS_CV_Blob_1.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE EXTERNAL DATA SOURCE [EXTDS_CV_Blob]
-    WITH (
-    TYPE = BLOB_STORAGE,
-    LOCATION = N'https://cvappstorage.blob.core.windows.net/api-json',
-    CREDENTIAL = [SC_SAS_CV_APP]
-    );
-
