23684c9eac91f93809c04a5c9e4f7a9533f5135d
Groen, Dirk <dgroen@ilionx.com>
2020-12-18 15:23:25 +0100

    delete external resources

--- cv-app-sqldb/External Resources/EXTDS_CV_Blob_ID.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE EXTERNAL DATA SOURCE [EXTDS_CV_Blob_ID]
-    WITH (
-    TYPE = BLOB_STORAGE,
-    LOCATION = N'https://cvappstorage.blob.core.windows.net/api-json-id',
-    CREDENTIAL = [SC_SAS_CV_APP]
-    );
-
