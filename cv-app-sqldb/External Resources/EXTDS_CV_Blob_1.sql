cdbb218e6c6e17ebe50bac6074290259cd550b00
Groen, Dirk <dgroen@ilionx.com>
2020-12-18 13:17:04 +0100

    extra check in

---
+++ cv-app-sqldb/External Resources/EXTDS_CV_Blob_1.sql
@@ -0,0 +1,7 @@
+﻿CREATE EXTERNAL DATA SOURCE [EXTDS_CV_Blob]
+    WITH (
+    TYPE = BLOB_STORAGE,
+    LOCATION = N'https://cvappstorage.blob.core.windows.net/api-json',
+    CREDENTIAL = [SC_SAS_CV_APP]
+    );
+
---
+++ cv-app-sqldb/External Resources/EXTDS_CV_Blob_ID_1.sql
@@ -0,0 +1,7 @@
+﻿CREATE EXTERNAL DATA SOURCE [EXTDS_CV_Blob_ID]
+    WITH (
+    TYPE = BLOB_STORAGE,
+    LOCATION = N'https://cvappstorage.blob.core.windows.net/api-json-id',
+    CREDENTIAL = [SC_SAS_CV_APP]
+    );
+
